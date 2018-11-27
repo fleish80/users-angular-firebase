107923d8c255425edea5e32f18df9f6e9ed4b93f
Dmitry  Fleish <i5z@bhpintune.onmicrosoft.com>
2018-11-27 21:40:19 +0200

    adding list

--- client/src/users/users.module.ts
+++ client/src/users/users.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersComponent } from './users.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
-  declarations: [UsersComponent, RegistrationComponent],
+  declarations: [UsersComponent, RegistrationComponent, ListComponent],
   imports: [
     CommonModule
   ]
