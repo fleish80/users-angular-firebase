17b3400248dda178aa8f356bca3362c00ea6ff9f
fleish80 <Fleish.D@gmail.com>
2018-12-22 21:57:44 +0200

    error pipe

--- client/src/shared/user-validators/error-key.pipe.ts
+++ client/src/shared/user-validators/error-key.pipe.ts
@@ -1,10 +1,11 @@
 import { Pipe } from "@angular/core";
+import { ValidationErrors } from "@angular/forms";
 
 @Pipe({
     name: 'error-key'
 })
 export class ErrorKeyPipe {
-    transform(obj) {
+    transform(obj: ValidationErrors) {
         var keys = Object.keys(obj);
         if (keys && keys.length > 0) {
             return keys[0];
---
+++ client/src/users/registration/error-messages.ts
@@ -0,0 +1,13 @@
+export const errorMessage = {
+    firstName: {
+        required: 'First name is required',
+        textOnly: 'First name contains invalid chars'
+    },
+    lastName: {
+        required: 'Last name is required',
+        textOnly: 'Last name contains invalid chars'
+    },
+    area : {
+        requiredIfHasValue: 'Must contain a value if there is a value in a phone number'
+    }
+}
